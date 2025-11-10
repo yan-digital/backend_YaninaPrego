14010d7ae1e80e7715f9ec469af39ed0e83969d2
Yani <yaninaprego@gmail.com>
2025-11-10 09:58:34 -0300

    primer entrega

---
+++ backProject/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import productsRouter from './routers/products.router.js'
+import cartsRouter from './routers/carts.router.js';
+
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+
+app.listen(PORT, ()=>{
+  console.log(`servidor en el puerto ${PORT}`)
+});
